d5e1641fe3e23a9e8374d02e9736461793a00f6c
Rich Harris <richard.a.harris@gmail.com>
2017-06-12 15:51:31 -0400

    use prettier

--- src/index.js
+++ src/index.js
@@ -4,24 +4,26 @@ import { createFilter, makeLegalIdentifier } from 'rollup-pluginutils';
 
 const ext = /\.ya?ml$/;
 
-export default function yaml ( options = {} ) {
-	const filter = createFilter( options.include, options.exclude );
+export default function yaml(options = {}) {
+	const filter = createFilter(options.include, options.exclude);
 
 	return {
 		name: 'yaml',
 
-		transform ( yaml, id ) {
-			if ( !ext.test( id ) ) return null;
-			if ( !filter( id ) ) return null;
+		transform(yaml, id) {
+			if (!ext.test(id)) return null;
+			if (!filter(id)) return null;
 
-			const data = YAML.load( yaml );
-			const keys = Object.keys( data ).filter( key => key === makeLegalIdentifier( key ) );
+			const data = YAML.load(yaml);
+			const keys = Object.keys(data).filter(
+				key => key === makeLegalIdentifier(key)
+			);
 
-			let code = `var data = ${toSource( data )};\n\n`;
+			let code = `var data = ${toSource(data)};\n\n`;
 
-			const exports = [ 'export default data;' ].concat(
-				keys.map( key => `export var ${key} = data.${key};` )
-			).join( '\n' );
+			const exports = ['export default data;']
+				.concat(keys.map(key => `export var ${key} = data.${key};`))
+				.join('\n');
 
 			return {
 				code: code + exports,
--- test/test.js
+++ test/test.js
@@ -1,63 +1,72 @@
-var assert = require( 'assert' );
-var rollup = require( 'rollup' );
-var yaml = require( '..' );
-var npm = require( 'rollup-plugin-node-resolve' );
-var yamlParser = require( 'js-yaml' )
+var assert = require('assert');
+var rollup = require('rollup');
+var yaml = require('..');
+var npm = require('rollup-plugin-node-resolve');
+var yamlParser = require('js-yaml');
 
-require( 'source-map-support' ).install();
+require('source-map-support').install();
 
-process.chdir( __dirname );
+process.chdir(__dirname);
 
-function executeBundle ( bundle ) {
+function executeBundle(bundle) {
 	var generated = bundle.generate();
 	var code = generated.code;
 
-	var fn = new Function( 'assert', code );
-	fn( assert );
+	var fn = new Function('assert', code);
+	fn(assert);
 }
 
-describe( 'rollup-plugin-yaml', function () {
-
+describe('rollup-plugin-yaml', function() {
 	// Tests YAML spec conformance from https://github.com/connec/yaml-spec/blob/master/spec.json
 	// Just making sure the underlying YAML parser isn't crap
-	var fs = require('fs')
-	var path = require('path')
-	var spec = JSON.parse(fs.readFileSync( path.join(__dirname, 'spec.json'), 'utf8' ))
-	for(var s in spec) {
-		it( 'supports spec: ' + s, function() {
-			for(var t in spec[s]) {
-				var test = spec[s][t]
-				var result = yamlParser.load(test.yaml)
-				assert.deepStrictEqual(result, test.result)
+	var fs = require('fs');
+	var path = require('path');
+	var spec = JSON.parse(
+		fs.readFileSync(path.join(__dirname, 'spec.json'), 'utf8')
+	);
+	for (var s in spec) {
+		it('supports spec: ' + s, function() {
+			for (var t in spec[s]) {
+				var test = spec[s][t];
+				var result = yamlParser.load(test.yaml);
+				assert.deepStrictEqual(result, test.result);
 			}
 		});
 	}
 
-	it( 'converts yaml', function () {
-		return rollup.rollup({
-			entry: 'samples/basic/main.js',
-			plugins: [ yaml() ]
-		}).then( executeBundle );
+	it('converts yaml', function() {
+		return rollup
+			.rollup({
+				entry: 'samples/basic/main.js',
+				plugins: [yaml()]
+			})
+			.then(executeBundle);
 	});
 
-	it( 'converts yml', function () {
-		return rollup.rollup({
-			entry: 'samples/yml/main.js',
-			plugins: [ yaml() ]
-		}).then( executeBundle );
+	it('converts yml', function() {
+		return rollup
+			.rollup({
+				entry: 'samples/yml/main.js',
+				plugins: [yaml()]
+			})
+			.then(executeBundle);
 	});
 
-	it( 'generates named exports', function () {
-		return rollup.rollup({
-			entry: 'samples/named/main.js',
-			plugins: [ yaml() ]
-		}).then( executeBundle );
+	it('generates named exports', function() {
+		return rollup
+			.rollup({
+				entry: 'samples/named/main.js',
+				plugins: [yaml()]
+			})
+			.then(executeBundle);
 	});
 
-	it( 'resolves extensionless imports in conjunction with npm plugin', function () {
-		return rollup.rollup({
-			entry: 'samples/extensionless/main.js',
-			plugins: [ npm({ extensions: [ '.js', '.yaml' ]}), yaml() ]
-		}).then( executeBundle );
+	it('resolves extensionless imports in conjunction with npm plugin', function() {
+		return rollup
+			.rollup({
+				entry: 'samples/extensionless/main.js',
+				plugins: [npm({ extensions: ['.js', '.yaml'] }), yaml()]
+			})
+			.then(executeBundle);
 	});
 });
